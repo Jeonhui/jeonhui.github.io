af4530c792b61df8e9e1aac72f086236a2b80456
Jeonhui <dlwjsgml02@naver.com>
2022-06-22 12:22:33 +0900

    portfolio

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Contact() {
+    const Footer = styled.div`
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background-color: rgb(230, 230, 230);
+      width: 100%;
+      height: 50%;
+    `
+    return (
+        <div className="Contact">
+            <Footer></Footer>
+        </div>
+    );
+}
