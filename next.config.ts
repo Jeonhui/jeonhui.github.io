dd6b1f6254024d94c77cd6a55338192ab1a1644e
Jeonhui Lee <48307153+Jeonhui@users.noreply.github.com>
2025-02-23 14:46:29 +0900

    package: vanilla-extract

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type {NextConfig} from "next";
+import {createVanillaExtractPlugin} from "@vanilla-extract/next-plugin";
+
+const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
     /* config options here */
@@ -9,4 +12,4 @@ const nextConfig: NextConfig = {
     distDir: "out",
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
